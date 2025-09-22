c4e80a3aa6f5bfc3fec240ef14ac02afbe10ebd7
dickxumamba <xuruoyu27@gmail.com>
2025-09-22 09:23:27 +0800

    updated,finished the functions

--- lab13.cpp
+++ lab13.cpp
@@ -12,9 +12,9 @@ and printing them. Additional operations include finding min/max times, swapping
 filling arrays, and searching for specific values. In our lab13, we will implement the same functionality, but using vector instead.
 */ 
 
-void readFile(); // create a function to read the external file and move the data in it into the array
-void extractTop15(); 
-void print_top15();
+void readFile(const string& filename, vector<double>& all50_records); // create a function to read the external file and move the data in it into the array
+void extractTop15(const vector<double>& all50_records, vector<double>& top15_records); 
+void print_top15(const vector<double>& top15_records);
 
 int main() {
 
@@ -29,8 +29,8 @@ void readFile(const string& filename, vector<double>& all50_records){
     ifstream fin(filename); // this function reads 50 marks from a text file into all50_records
     double mark;
     int i = 0;
-    while (fin >> mark && i < static_cast<int>(all50_records.size())) {
-        all50_records.at(i) = mark;  // bounds-checked
+    while (fin >> mark) {
+        all50_records.push_back(mark);  // bounds-checked
         i++;
     }
     fin.close();
@@ -38,13 +38,17 @@ void readFile(const string& filename, vector<double>& all50_records){
 
 // This function sorts the copy of the 50 marks and copy the first 15 (fastest) into top15_records
 void extractTop15(const vector<double>& all50_records, vector<double>& top15_records) {
+    top15_records.clear(); // every time call this function, clear the rank(which means all elements in it) first
     vector<double> temp = all50_records;   // copy the all50_records
     sort(temp.begin(), temp.end());                   
     for (int i = 0; i < 15; i++) {
-        top15_records.at(i) = temp.at(i);
+        top15_records.push_back(temp.at(i));
     }
 }
 
-void print_top15(){
-
+void print_top15(const vector<double>& top15_records){
+    for (int i = 0; i < 15; ++i) {
+    cout << (i+1) << ". " << top15_records[i] <<endl;
+}
+    cout <<endl;
 }
